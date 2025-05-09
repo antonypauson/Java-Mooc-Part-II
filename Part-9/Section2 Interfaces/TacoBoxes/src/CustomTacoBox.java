5d64436f62bef02ced34b9f36bcf669187d9b3e9
antonypauson <13antonysun@gmail.com>
2025-05-09 21:54:25 +0530

    taco box

---
+++ Part-9/Section2 Interfaces/TacoBoxes/src/CustomTacoBox.java
@@ -0,0 +1,7 @@
+public class CustomTacoBox implements TacoBox {
+    private int tacos;
+
+    public CustomTacoBox(int tacos) {
+        this.tacos = tacos;
+    }
+}
---
+++ Part-9/Section2 Interfaces/TacoBoxes/src/TripleTacoBox.java
@@ -0,0 +1,8 @@
+public class TripleTacoBox implements TacoBox {
+    private int tacos;
+
+    public TripleTacoBox() {
+        this.tacos = 3;
+    }
+
+}
