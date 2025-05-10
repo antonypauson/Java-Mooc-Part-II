43309a003e540187c26a89912927879b3842a9e9
antonypauson <13antonysun@gmail.com>
2025-05-10 13:32:33 +0530

    Reading Files Per Line

---
+++ Part-10/Section1 Handling Collections as Streams/src/PersonNew.java
@@ -0,0 +1,21 @@
+public class PersonNew {
+    private String name;
+    private int birthYear;
+
+    public PersonNew(String name, int birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getBirthYear() {
+        return this.birthYear;
+    }
+
+    public String toString() {
+        return this.name + " (" + this.birthYear + ")";
+    }
+}
