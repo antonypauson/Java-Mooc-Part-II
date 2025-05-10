ff9bbb05808f0b9f2867991938bd083f6a3bbef1
antonypauson <13antonysun@gmail.com>
2025-05-10 10:16:20 +0530

    part 2 of animals

---
+++ Part-9/Section3 Object Polymorphism/Animals/src/Dog.java
@@ -0,0 +1,23 @@
+public class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    public Dog() {
+        super("Dog");
+    }
+
+    public void bark() {
+        System.out.println(this.getName()+ " barks");
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.bark();
+        dog.eat();
+
+        Dog fido = new Dog("Fido");
+        fido.bark();
+    }
+}
