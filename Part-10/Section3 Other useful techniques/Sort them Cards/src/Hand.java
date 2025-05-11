c66bb2905e17d9007630cf8505813ccf1345e1e4
antonypauson <13antonysun@gmail.com>
2025-05-11 23:47:50 +0530

    part 4 of sort them cards!

--- Part-10/Section3 Other useful techniques/Sort them Cards/src/Hand.java
+++ Part-10/Section3 Other useful techniques/Sort them Cards/src/Hand.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Hand {
+public class Hand implements Comparable<Hand> {
 
     private List<Card> cards;
 
@@ -12,6 +12,20 @@ public class Hand {
 
     public void add(Card card) {
         this.cards.add(card);
+    }
+
+    @Override
+    public int compareTo(Hand hand) {
+        int value1 =
+                this.cards.stream()
+                        .map(card -> card.getValue())
+                        .reduce(0, (sum, each) -> sum += each);
+        int value2 =
+                hand.cards.stream()
+                        .map(card -> card.getValue())
+                        .reduce(0, (sum, each) -> sum += each);
+        return value1 - value2;
+
     }
 
     public void sort() {
@@ -25,14 +39,39 @@ public class Hand {
     }
 
     public static void main(String[] args) {
-        Hand hand = new Hand();
+//        Hand hand = new Hand();
+//
+//        hand.add(new Card(2, Suit.DIAMOND));
+//        hand.add(new Card(14, Suit.SPADE));
+//        hand.add(new Card(12, Suit.HEART));
+//        hand.add(new Card(2, Suit.SPADE));
+//
+//        hand.sort();
+//        hand.print();
 
-        hand.add(new Card(2, Suit.DIAMOND));
-        hand.add(new Card(14, Suit.SPADE));
-        hand.add(new Card(12, Suit.HEART));
-        hand.add(new Card(2, Suit.SPADE));
+        Hand hand1 = new Hand();
 
-        hand.sort();
-        hand.print();
+        hand1.add(new Card(2, Suit.DIAMOND));
+        hand1.add(new Card(14, Suit.SPADE));
+        hand1.add(new Card(12, Suit.HEART));
+        hand1.add(new Card(2, Suit.SPADE));
+
+        Hand hand2 = new Hand();
+
+        hand2.add(new Card(11, Suit.DIAMOND));
+        hand2.add(new Card(11, Suit.SPADE));
+        hand2.add(new Card(11, Suit.HEART));
+
+        int comparison = hand1.compareTo(hand2);
+
+        if (comparison < 0) {
+            System.out.println("better hand is");
+            hand2.print();
+        } else if (comparison > 0){
+            System.out.println("better hand is");
+            hand1.print();
+        } else {
+            System.out.println("hands are equal");
+        }
     }
 }
