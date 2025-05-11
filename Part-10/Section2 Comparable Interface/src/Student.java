456906ee8cc4a79a90d8164be6dd1c5bf5f3995c
antonypauson <13antonysun@gmail.com>
2025-05-11 18:58:27 +0530

    students on alphabetical order

--- Part-10/Section2 Comparable Interface/src/Student.java
+++ Part-10/Section2 Comparable Interface/src/Student.java
@@ -17,10 +17,11 @@ public class Student implements Comparable<Student> {
     }
 
     @Override
-    public int compareTo(Student o) {
-        return 0;
+    public int compareTo(Student student) {
+        return this.getName().compareTo(student.getName());
     }
 
     public static void main(String[] args) {
+
     }
 }
