51cf64e79f79c93ec386b24f7da9f3be9ca61067
antonypauson <13antonysun@gmail.com>
2025-05-11 22:26:37 +0530

    part 1: day of week

---
+++ Part-10/Section3 Other useful techniques/src/Checker.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Checker {
+
+    public static boolean isDayOfWeek(String string) {
+        if (string.matches(
+                "mon|tue|wed|thu|fri|sat|sun"
+        ))    {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter a string:");
+            String input = scanner.nextLine();
+
+            if (input.isEmpty()) {
+                break;
+            }
+
+            if (isDayOfWeek(input)) {
+                System.out.println("The form is correct");
+            } else {
+                System.out.println("The form is incorrect");
+            }
+        }
+    }
+
+}
--- Part-10/Section3 Other useful techniques/src/RegularExpression.java
+++ Part-10/Section3 Other useful techniques/src/RegularExpression.java
@@ -3,14 +3,40 @@ import java.util.Scanner;
 public class RegularExpression {
     public static void main(String[] args) {
         System.out.println("Provide an indian number:");
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        String number = scanner.nextLine();
+//
+//        if (number.matches("91[6-9][0-9]{9}")) {
+//            System.out.println("Correct Indian Number");
+//        } else {
+//            System.out.println("Incorrect Indian Number");
+//        }
 
-        String number = scanner.nextLine();
+        //vertical line optional
+        System.out.println("000".matches("00|11|000|111"));
+        System.out.println("1".matches("00|11|000|111"));
 
-        if (number.matches("91[6-9][0-9]{9}")) {
-            System.out.println("Correct Indian Number");
-        } else {
-            System.out.println("Incorrect Indian Number");
-        }
+        //parenthesis
+        //only allow 00001 or 00000
+        System.out.println("00000".matches("0000(0|1)"));
+        System.out.println("car".matches("car(|s|)"));
+
+        //quantifiers
+        //* 0 to n times
+        System.out.println("trolololololololo".matches("tro(lo)*"));
+        //+ 1 to n times
+        System.out.println("tro".matches("tro(lo)+")); //lo should be atleast one time
+        System.out.println("trolo".matches("tro(lo)*"));//lo can be 0 times
+        //? 0 or 1 times
+        System.out.println("You have to accidentally delete the whole thing"
+                .matches("You have to accidentally (delete)? the whole thing"));
+        //{n} repeats that many times
+        System.out.println("1010".matches("(10){2}"));
+        //{a,b} repeats a to b times
+        System.out.println("101010".matches("(10){1,3}"));
+        //{a,} repeats a to any times
+        System.out.println("1010101010".matches("(10){1,}"));
+
     }
 }
