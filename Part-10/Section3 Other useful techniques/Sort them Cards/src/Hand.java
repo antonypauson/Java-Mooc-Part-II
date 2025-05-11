a51cdc8d776b06bd011ff5ebafe6dbf678c178cd
antonypauson <13antonysun@gmail.com>
2025-05-11 23:39:54 +0530

    part 2 of sort them cards!

---
+++ Part-10/Section3 Other useful techniques/Sort them Cards/src/Hand.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+
+    private List<Card> cards;
+
+    public Hand() {
+        this.cards = new ArrayList<>();
+    }
+
+    public void add(Card card) {
+        this.cards.add(card);
+    }
+
+    public void print() {
+        for (Card card : cards) {
+            System.out.println(card);
+        }
+    }
+
+    public static void main(String[] args) {
+        Hand hand = new Hand();
+
+        hand.add(new Card(2, Suit.DIAMOND));
+        hand.add(new Card(14, Suit.SPADE));
+        hand.add(new Card(12, Suit.HEART));
+        hand.add(new Card(2, Suit.SPADE));
+
+        hand.print();
+    }
+}
