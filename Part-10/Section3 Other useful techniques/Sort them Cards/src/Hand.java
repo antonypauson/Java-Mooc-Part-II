5592dbc1379ced9de605582cae6784321742c631
antonypauson <13antonysun@gmail.com>
2025-05-11 23:54:09 +0530

    part 6  of sort them cards!

--- Part-10/Section3 Other useful techniques/Sort them Cards/src/Hand.java
+++ Part-10/Section3 Other useful techniques/Sort them Cards/src/Hand.java
@@ -38,6 +38,11 @@ public class Hand implements Comparable<Hand> {
         }
     }
 
+    public void sortBySuit() {
+        BySuitInValueOrder sortBySuitSorter = new BySuitInValueOrder();
+        Collections.sort(cards, sortBySuitSorter);
+    }
+
     public static void main(String[] args) {
 //        Hand hand = new Hand();
 //
@@ -49,29 +54,42 @@ public class Hand implements Comparable<Hand> {
 //        hand.sort();
 //        hand.print();
 
-        Hand hand1 = new Hand();
+//        Hand hand1 = new Hand();
+//
+//        hand1.add(new Card(2, Suit.DIAMOND));
+//        hand1.add(new Card(14, Suit.SPADE));
+//        hand1.add(new Card(12, Suit.HEART));
+//        hand1.add(new Card(2, Suit.SPADE));
+//
+//        Hand hand2 = new Hand();
+//
+//        hand2.add(new Card(11, Suit.DIAMOND));
+//        hand2.add(new Card(11, Suit.SPADE));
+//        hand2.add(new Card(11, Suit.HEART));
+//
+//        int comparison = hand1.compareTo(hand2);
+//
+//        if (comparison < 0) {
+//            System.out.println("better hand is");
+//            hand2.print();
+//        } else if (comparison > 0){
+//            System.out.println("better hand is");
+//            hand1.print();
+//        } else {
+//            System.out.println("hands are equal");
+//        }
 
-        hand1.add(new Card(2, Suit.DIAMOND));
-        hand1.add(new Card(14, Suit.SPADE));
-        hand1.add(new Card(12, Suit.HEART));
-        hand1.add(new Card(2, Suit.SPADE));
+        Hand hand = new Hand();
 
-        Hand hand2 = new Hand();
+        hand.add(new Card(12, Suit.HEART));
+        hand.add(new Card(4, Suit.SPADE));
+        hand.add(new Card(2, Suit.DIAMOND));
+        hand.add(new Card(14, Suit.SPADE));
+        hand.add(new Card(7, Suit.HEART));
+        hand.add(new Card(2, Suit.SPADE));
 
-        hand2.add(new Card(11, Suit.DIAMOND));
-        hand2.add(new Card(11, Suit.SPADE));
-        hand2.add(new Card(11, Suit.HEART));
+        hand.sortBySuit();
 
-        int comparison = hand1.compareTo(hand2);
-
-        if (comparison < 0) {
-            System.out.println("better hand is");
-            hand2.print();
-        } else if (comparison > 0){
-            System.out.println("better hand is");
-            hand1.print();
-        } else {
-            System.out.println("hands are equal");
-        }
+        hand.print();
     }
 }
