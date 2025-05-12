16a49b9426c89244dfd709684c0e3898fefe598e
antonypauson <13antonysun@gmail.com>
2025-05-12 16:54:51 +0530

    flight control -> Airplane

--- Part-11/Section2 Packages/Flight Control/src/flightControl/domain/Airplane.java
+++ Part-11/Section2 Packages/Flight Control/src/flightControl/domain/Airplane.java
@@ -1,4 +1,25 @@
 package flightControl.domain;
 
 public class Airplane {
+
+    private String id;
+    private int capacity;
+
+    public Airplane(String id, int capacity) {
+        this.id = id;
+        this.capacity = capacity;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + " (" + this.capacity + " capacity)";
+    }
 }
