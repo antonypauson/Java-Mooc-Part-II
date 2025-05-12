1d4fa71527e41eb0c69891a68e3825982cc5c918
antonypauson <13antonysun@gmail.com>
2025-05-12 16:42:34 +0530

    access modifiers

--- Part-11/Section2 Packages/src/library/domain/Book.java
+++ Part-11/Section2 Packages/src/library/domain/Book.java
@@ -10,4 +10,13 @@ public class Book {
     public String getName() {
         return this.name;
     }
+
+    public static class Program {
+        public static void main(String[] args) {
+    //        System.out.println("Hello packageworld!");
+            Book book = new Book("the ABCs of packages");
+            System.out.println("Hello package world " + book.getName());
+        }
+
+    }
 }
---
+++ Part-11/Section2 Packages/src/library/ui/UserInterface.java
@@ -0,0 +1,22 @@
+package library.ui;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    private Scanner scanner;
+
+    public UserInterface(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void start() {
+        printTitle();
+    }
+
+    void printTitle() {
+        System.out.println("***********");
+        System.out.println("* LIBRARY *");
+        System.out.println("***********");
+
+    }
+}
