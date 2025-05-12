c826a4b047570a8bdf2adea88772fc707797624e
antonypauson <13antonysun@gmail.com>
2025-05-12 22:22:44 +0530

    part 2 of Saveable Dictionary

--- Part-11/Section4 Processing Files/src/dictionary/SaveableDictionary.java
+++ Part-11/Section4 Processing Files/src/dictionary/SaveableDictionary.java
@@ -14,32 +14,52 @@ public class SaveableDictionary {
         this.words.putIfAbsent(word, translation);
     }
 
+    public void delete(String word) {
+        String itemReturned = "";
+        if (this.words.containsKey(word)) {
+            itemReturned = word;
+        } else if (this.words.containsValue(word)) {
+            for (String key : words.keySet()) {
+                if (this.words.get(key).equals(word)) {
+                    itemReturned = key;
+                }
+            }
+        } else {
+            itemReturned = null;
+        }
+
+        this.words.remove(itemReturned);
+    }
+
     public String translate(String word) {
-        String translateWord = "";
-       if (this.words.containsKey(word)) {
-           translateWord = this.words.get(word);
-       } else if (this.words.containsValue(word)) {
-           for (String meaning : this.words.keySet()) {
-               if (this.words.get(meaning).equals(word)) {
-                   translateWord = meaning;
-               }
-           }
-       } else {
-           translateWord = null;
-       }
-       return translateWord;
+        String itemReturned = "";
+        if (this.words.containsKey(word)) {
+            itemReturned = this.words.get(word);
+        } else if (this.words.containsValue(word)) {
+            for (String key : this.words.keySet()) {
+                if (this.words.get(key).equals(word)) {
+                    itemReturned = key;
+                }
+            }
+        } else {
+            itemReturned = null;
+        }
+        return itemReturned;
     }
 
     public static void main(String[] args) {
         SaveableDictionary dictionary = new SaveableDictionary();
         dictionary.add("apina", "monkey");
         dictionary.add("banaani", "banana");
-        dictionary.add("apina", "apfe");
+        dictionary.add("ohjelmointi", "programming");
+        dictionary.delete("apina");
+        dictionary.delete("banana");
 
         System.out.println(dictionary.translate("apina"));
         System.out.println(dictionary.translate("monkey"));
-        System.out.println(dictionary.translate("ohjelmointi"));
         System.out.println(dictionary.translate("banana"));
+        System.out.println(dictionary.translate("banaani"));
+        System.out.println(dictionary.translate("ohjelmointi"));
     }
 }
 
