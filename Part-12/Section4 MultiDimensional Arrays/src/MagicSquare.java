a8f64a3822ab4b223892e1e81156ffb5a57b2465
antonypauson <13antonysun@gmail.com>
2025-05-13 20:54:50 +0530

    part 1, magic rows -> sum of rows

---
+++ Part-12/Section4 MultiDimensional Arrays/src/MagicSquare.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MagicSquare {
+    private int[][] square;
+    private Scanner scanner;
+
+    public MagicSquare() {
+        square = new int[3][3];
+        scanner = new Scanner(System.in);
+    }
+
+    public void addNumbers() {
+        System.out.println("Enter numbers: ");
+        for (int row = 0; row < square.length; row++) {
+            for (int col = 0; col < square[row].length; col++) {
+                int num = scanner.nextInt();
+                square[row][col] = num;
+            }
+        }
+        printMagicSquare();
+    }
+
+    public ArrayList<Integer> sumsOfRows() {
+        ArrayList<Integer> listOfSums = new ArrayList<>();
+        for (int row = 0; row < square.length; row++) {
+            int sum = 0;
+            for (int col = 0; col < square[row].length; col++) {
+                sum += square[row][col];
+            }
+            listOfSums.add(sum);
+        }
+        return listOfSums;
+    }
+
+    public void printMagicSquare() {
+        System.out.println("Printing: ");
+        for (int row = 0; row < square.length; row++) {
+            for (int col = 0; col < square[row].length; col++) {
+                System.out.print(square[row][col]);
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        MagicSquare magicSquare = new MagicSquare();
+        magicSquare.addNumbers();
+        System.out.println(magicSquare.sumsOfRows());
+    }
+
+}
