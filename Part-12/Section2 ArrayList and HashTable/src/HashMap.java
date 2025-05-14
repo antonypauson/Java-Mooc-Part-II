e831e080f306ecd8594d7a371bc24efb24f2bdab
antonypauson <13antonysun@gmail.com>
2025-05-14 17:43:37 +0530

    hashmap get(key)

--- Part-12/Section2 ArrayList and HashTable/src/HashMap.java
+++ Part-12/Section2 ArrayList and HashTable/src/HashMap.java
@@ -6,4 +6,20 @@ public class HashMap <K,V>{
         this.values = new List[32];
         this.firstFreeIndex = 0;
     }
+
+    public V get (K key) {
+        int hashValue = Math.abs(key.hashCode() % this.values.length);
+        if (this.values[hashValue] == null) {
+            return null;
+        }
+
+        List<Pair<K, V>> valuesAtIndex = this.values[hashValue];
+
+        for (int i = 0; i < valuesAtIndex.size(); i++) {
+            if (valuesAtIndex.value(i).getKey().equals(key)) {
+                return valuesAtIndex.value(i).getValue();
+            }
+        }
+        return null;
+    }
 }
