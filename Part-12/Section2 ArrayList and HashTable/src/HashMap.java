ac9ef47a138199ee9a06a68bba83d85c7a7ded87
antonypauson <13antonysun@gmail.com>
2025-05-14 17:50:36 +0530

    hashmap add(key,value)

--- Part-12/Section2 ArrayList and HashTable/src/HashMap.java
+++ Part-12/Section2 ArrayList and HashTable/src/HashMap.java
@@ -7,6 +7,29 @@ public class HashMap <K,V>{
         this.firstFreeIndex = 0;
     }
 
+    public void add(K key, V value) {
+        int hashValue = Math.abs(key.hashCode() % values.length);
+        if (values[hashValue] == null) {
+            values[hashValue] = new List<>();
+        }
+
+        List<Pair<K, V>> valuesAtIndex = values[hashValue];
+
+        int index = -1;
+        for (int i = 0; i < valuesAtIndex.size(); i++) {
+            if (valuesAtIndex.value(i).getKey().equals(key)) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index < 0) {
+            valuesAtIndex.add(new Pair<>(key,value));
+        } else {
+            valuesAtIndex.value(index).setValue(value);
+        }
+    }
+
     public V get (K key) {
         int hashValue = Math.abs(key.hashCode() % this.values.length);
         if (this.values[hashValue] == null) {
