0c9f334e0e577ec70bf9e5a5025ca4c7ad363480
antonypauson <13antonysun@gmail.com>
2025-05-14 21:31:22 +0530

    borderpane exercise

--- Part-13/src/application/JavaFxApplication.java
+++ Part-13/src/application/JavaFxApplication.java
@@ -14,11 +14,13 @@ public class JavaFxApplication extends Application{
 
     @Override
     public void start(Stage window)  {
+
         BorderPane layout = new BorderPane();
-        layout.setTop(new Label("Top"));
-        layout.setBottom(new Label("Bottom"));
-        layout.setLeft(new Label("Left"));
-        layout.setRight(new Label("Right"));
+        layout.setTop(new Label("NORTH"));
+        layout.setBottom(new Label("SOUTH"));
+        layout.setLeft(new Label("WEST"));
+        layout.setRight(new Label("EAST"));
+        layout.setCenter(new Label("CENTER"));
 
         Scene view = new Scene(layout);
 
