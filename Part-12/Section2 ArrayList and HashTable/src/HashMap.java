47f1bb6cd6781dea5ced97f6ee0d4640644baac1
antonypauson <13antonysun@gmail.com>
2025-05-14 17:55:39 +0530

    hashmap add(key,value) simpler

--- Part-12/Section2 ArrayList and HashTable/src/HashMap.java
+++ Part-12/Section2 ArrayList and HashTable/src/HashMap.java
@@ -7,24 +7,29 @@ public class HashMap <K,V>{
         this.firstFreeIndex = 0;
     }
 
-    public void add(K key, V value) {
+    private List<Pair<K,V>> getListBasedOnKey(K key) {
         int hashValue = Math.abs(key.hashCode() % values.length);
         if (values[hashValue] == null) {
             values[hashValue] = new List<>();
         }
+        return values[hashValue];
+    }
 
-        List<Pair<K, V>> valuesAtIndex = values[hashValue];
-
-        int index = -1;
-        for (int i = 0; i < valuesAtIndex.size(); i++) {
-            if (valuesAtIndex.value(i).getKey().equals(key)) {
-                index = i;
-                break;
+    private int getIndexOfKey(List<Pair<K,V>> myList, K key) {
+        for (int i = 0; i < myList.size(); i++) {
+            if (myList.value(i).getKey().equals(key)) {
+                return i;
             }
         }
+        return -1;
+    }
+    public void add(K key, V value) {
+        List<Pair<K, V>> valuesAtIndex = getListBasedOnKey(key);
+        int index = getIndexOfKey(valuesAtIndex, key);
 
         if (index < 0) {
-            valuesAtIndex.add(new Pair<>(key,value));
+            valuesAtIndex.add(new Pair<>(key, value));
+            this.firstFreeIndex++;
         } else {
             valuesAtIndex.value(index).setValue(value);
         }
