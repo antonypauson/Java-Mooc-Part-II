c0c8eaea50cea42e13f72e12c8b25b9a4886d55c
antonypauson <13antonysun@gmail.com>
2025-05-14 21:19:22 +0530

    adding button and label

--- Part-13/src/application/JavaFxApplication.java
+++ Part-13/src/application/JavaFxApplication.java
@@ -2,11 +2,28 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+
+
 public class JavaFxApplication extends Application{
 
     @Override
     public void start(Stage window)  {
+        Button button = new Button("This is a button");
+        Label textComponent = new Label("Text element: ");
+
+        FlowPane componentGroup = new FlowPane();
+        componentGroup.getChildren().add(button);
+        componentGroup.getChildren().add(textComponent);
+
+        Scene scene = new Scene(componentGroup);
+
+
         window.setTitle("hello world");
+        window.setScene(scene);
         window.show();
     }
 
