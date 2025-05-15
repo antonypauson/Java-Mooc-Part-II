a0770985a20140a75598d0fa91deb3446a191158
antonypauson <13antonysun@gmail.com>
2025-05-15 16:17:48 +0530

    button, event handler intro

--- Part-13/src/application/MyApplication.java
+++ Part-13/src/application/MyApplication.java
@@ -2,12 +2,27 @@ package application;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 
 public class MyApplication extends Application {
 
     @Override
     public void start(Stage window) {
+        Button button = new Button("Click here!");
+        BorderPane layout = new BorderPane();
+
+        button.setOnAction((event) -> {
+            System.out.println("Pressed the button!");
+        });
+
+        layout.setCenter(button);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
         window.setTitle("My first application");
+
         window.show();
     }
 
